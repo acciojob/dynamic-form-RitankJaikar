0def1f9c6b9a328a6559fc3a66b17da508abc67b
RitankJaikar <ritank.jaikar@gmail.com>
2025-01-04 20:14:21 +0530

    Submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,40 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
+let i=0;
+
 const App = () => {
+  const [inputIds, setInputIds] = useState([]);
+
+  function addField() {
+    setInputIds([...inputIds, ++i]);
+  }
+
+  console.log(inputIds);
+
+  function deletefield(id) {
+    const updatedInputIds = inputIds.filter(inputId => inputId !== id);
+    setInputIds(updatedInputIds);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <form>
+        {
+          inputIds.length
+          ?
+          inputIds.map(id => (
+            <div key={id}>
+              <input />
+              <button onClick={() => deletefield(id)}>Delete</button>
+            </div>
+          ))
+          :
+          <div>No fields from form</div>
+        }
+      </form>
+      <button onClick={addField}>Add Field</button>
     </div>
   )
 }
